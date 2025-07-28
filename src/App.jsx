fe375fb32c1eebfe7432236037d7baa076f152bd
isroiljondeveloper <isroiljonmusajonov@gmail.com>
2025-07-28 11:43:01 +0500

    atirlar

---
+++ src/App.jsx
@@ -0,0 +1,481 @@
+import React from "react";
+function App() {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="nac">
+          <div className="bash">
+            <img src="./img/image.png" alt="" />
+            <p className="gorod">Ваш город: Москва</p>
+          </div>
+          <div className="bonus">
+            <p className="gorod">Бонусы</p>
+            <p className="gorod">Документация</p>
+            <p className="gorod">О нас</p>
+            <div className="hon">
+              <img src="./img/phone.png" alt="" />
+              <p className="gorod">7(937) 136 - 77 - 66</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="katalog">
+        <div className="container">
+          <div className="people">
+            <img src="./img/par.png" alt="" />
+            <div className="dff">
+              <button className="gorodd">
+                <img src="./img/chizig.png" alt="" />
+                Каталог
+              </button>
+              <input
+                type="text"
+                placeholder="Найти парфюм.."
+                className="hello"
+              />
+            </div>
+            <div>
+              <img src="./img/odam.png" alt="" />
+              <img src="./img/bag.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="header">
+        <div className="container">
+          <div className="proo">
+            <h4 className="proda">
+              Оптовая продажа люксовой парфюмерии <br /> с маржой до 100% и
+              доставкой по РФ и СНГ
+            </h4>
+            <h5 className="bolle">
+              В наличии более <br /> 500 ароматов
+            </h5>
+          </div>
+          <img className="frame" src="./img/Frame 167.png" alt="" />
+        </div>
+      </div>
+
+      <div className="duhilar">
+        <div className="container">
+          <h6 className="aromat">Популярные ароматы</h6>
+          <div className="atirlar">
+            <div className="dlya">
+              <div>
+                <h6>Для неё</h6>
+              </div>
+              <div className="bom">
+                <p>Victoria Secret Bombshell</p>
+                <button>в корзину</button>
+              </div>
+            </div>
+            <div className="dlya2">
+              <div>
+                <h6>Для него</h6>
+              </div>
+              <div className="bom">
+                <p>Victoria Secret Bombshell</p>
+                <button>в корзину</button>
+              </div>
+            </div>
+            <div className="dlya3">
+              <div>
+                <h6>Унисекс</h6>
+              </div>
+              <div className="bom">
+                <p>Narcotiqe</p>
+                <button>в корзину</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <h6 className="aromat">Бонусы от покупок</h6>
+      </div>
+
+      <div className="bonus5">
+        <div className="container">
+          <div className="uznat">
+            <h5>Получайте кэшбэк за покупки</h5>
+            <p>Бонусные баллы за регистрацию и покупки</p>
+            <button>Узнать подробнее</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="but">
+        <div className="container">
+          <h6 className="aromat">Каталог</h6>
+          <div className="po">
+            <button className="fil">
+              Фильтры
+              <img src="./img/ikon.png" alt="" />
+            </button>
+            <button className="fill">
+              По популярности
+              <img src="./img/ikon1.png" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="cards">
+        <div className="container">
+          <div className="ss">
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+            <div className="atr">
+              <img src="./img/duh.png" alt="" />
+              <p>The devil is a loser by Mushfig</p>
+              <p>Объем мл.</p>
+              <div className="on">
+                <div className="boxx">
+                  <p className="yuz">10</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">30</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">50</p>
+                </div>
+                <div className="boxx">
+                  <p className="yuz">100</p>
+                </div>
+              </div>
+              <div className="most">
+                <p>Стоимость:</p>
+                <p>2 400,00 ₽</p>
+              </div>
+              <button className="bb">в корзину</button>
+            </div>
+          </div>
+          <div className="ikki">
+            <img src="./img/image copy.png" alt="" />
+            <img src="./img/image copy 2.png" alt="" />
+            <img src="./img/image copy 3.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="container">
+          <div className="kapalak">
+            <img src="./img/par.png" alt="" />
+          </div>
+
+          <div className="markaz">
+            <div className="gazz">
+              <div className="gaz">
+                <img src="./img/image copy 4.png" alt="" />
+                <p>
+                  г. Астрахань: ул. Свердлова, 106;ул. Победы <br /> 55А, пав.
+                  10.
+                </p>
+              </div>
+              <div className="gaz">
+                <img src="./img/image copy 5.png" alt="" />
+                <p>Ежедневно с 9:00 до 18:00</p>
+              </div>
+            </div>
+            <div className="gazz">
+              <div className="gaz">
+                <img src="./img/image copy 6.png" alt="" />
+                <p>Мы в Whatsapp</p>
+              </div>
+              <div className="gaz">
+                <img src="./img/image copy 7.png" alt="" />
+                <p>Мы в Telegram</p>
+              </div>
+              <div className="gaz">
+                <img src="./img/image copy 8.png" alt="" />
+                <p>parfumpomotivam@gmail.com</p>
+              </div>
+            </div>
+            <div className="gazz">
+              <div className="gaz">
+                <img src="./img/image copy 9.png" alt="" />
+                <p>
+                  ИП РАГИМОВА А.М.К. <br /> ИНН 510704693888
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="kapalak">
+          <p>
+            © Parfumpomotivam 2023 <br />
+            Политика конфиденциальности
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
